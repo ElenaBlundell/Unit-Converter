75b46433ef342a53f7558e31d12639d007107c14
ElenaKravtcova <57737705+ElenaKravtcova@users.noreply.github.com>
2022-08-24 22:46:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const meterFeet =  3.28084
 const literGallon =  0.264172
 const kiloPound =  2.20462
 
+const metersToFeet = document.getElementById("meters-to-feet");
+const feetToMeters = document.getElementById("feet-to-meters");
+const litersToGallons = document.getElementById("liters-to-gallons");
+const gallonsToLiters = document.getElementById("gallons-to-liters");
+const kilosToPounds = document.getElementById("kilos-to-pounds");
+const poundsToKilos = document.getElementById("pounds-to-kilos");
+
 convertBtn.addEventListener("click", function(){
     for (let i = 0; i < givenNumber.length; i++) {
           givenNumber[i].textContent = inputValue.value;
@@ -15,22 +22,13 @@ convertBtn.addEventListener("click", function(){
 } )
 
 function conversion(givenValue){
-    let metersToFeet = document.getElementById("meters-to-feet");
     metersToFeet.textContent = (givenValue * meterFeet).toFixed(3);
-
-    let feetToMeters = document.getElementById("feet-to-meters");
     feetToMeters.textContent = (givenValue / meterFeet).toFixed(3);
 
-    let litersToGallons = document.getElementById("liters-to-gallons");
     litersToGallons.textContent = (givenValue * literGallon).toFixed(3);
-
-    let gallonsToLiters = document.getElementById("gallons-to-liters");
     gallonsToLiters.textContent = (givenValue / literGallon).toFixed(3);
-
-    let kilosToPounds = document.getElementById("kilos-to-pounds");
+    
     kilosToPounds.textContent = (givenValue * kiloPound).toFixed(3);
-
-    let poundsToKilos = document.getElementById("pounds-to-kilos");
     poundsToKilos.textContent = (givenValue / kiloPound).toFixed(3);
 }
 
